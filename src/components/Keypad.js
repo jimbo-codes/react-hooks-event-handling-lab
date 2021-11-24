ef175c91908e24a2a342c25b3cfa1d08d953df89
James Burns <jrburns96@aol.com>
2021-11-24 11:17:35 -0500

    commit tree 36652f2a4a6f4c0a817287ebd4e35ccd7503f635

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+function Keypad(){
+    return(
+        <input onChange={watchIt} type="password" />
+    )
+}
+
+function watchIt(event){
+console.log('Entering password...')
+}
+export default Keypad
